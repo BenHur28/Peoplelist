8d687384d31816b5b60c993db6351b47aa689c6e
Ben Hur <bhur777@gmail.com>
2023-09-14 22:03:38 -0400

    create layout page

--- Peoplelist/Controllers/PersonController.cs
+++ Peoplelist/Controllers/PersonController.cs
@@ -4,6 +4,12 @@ namespace Peoplelist.Controllers
 {
     public class PersonController : Controller{
         public IActionResult Index(){
+            // ViewBag and ViewData can send data only from ControllerToView
+            ViewBag.greetings = "Hello World";
+            ViewData["greeting2"] = "I am using ViewData";
+
+            // TempData can send data from 1 controller method to another controller method
+            TempData["greeting3"] = "This is TempData";
             return View();
         }
     }
