2cd9837bc1b393e59229ee26c96976044fce226a
Ben Hur <bhur777@gmail.com>
2023-09-14 22:11:43 -0400

    add person model

---
+++ Peoplelist/Models/Domain/Person.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcCoreTutorial.Models.Domain
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+    }
+}
